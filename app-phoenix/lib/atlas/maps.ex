aea4bbddf4348d18fa79abc729b5f42a808afbc9
Eugene Burmakin <iamfrey@gmail.com>
2026-05-26 22:27:46 +0200

    feat(atlas-phoenix): Atlas.Maps.Geocode wrapper

--- app-phoenix/lib/atlas/maps.ex
+++ app-phoenix/lib/atlas/maps.ex
@@ -1,5 +1,5 @@
 defmodule Atlas.Maps do
   use Boundary,
     deps: [Req, Cachex, Logger, Task],
-    exports: [Result, Search, Reverse, Route, Transit]
+    exports: [Result, Search, Reverse, Route, Transit, Geocode]
 end
